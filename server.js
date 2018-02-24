b86c044240a7bba223c2e8a17533551a355a96c3
FilipElen <filipelengit@outlook.com>
2018-02-24 16:29:29 +0100

    PRODUCTION VERSION

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ morgan  = require('morgan'),
 mysql    = require('mysql'),
 bodyParser = require('body-parser');
 
-/*
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-*/
+/*
 var pool = mysql.createPool({
   multipleStatements: true,
   connectionLimit : 3,
@@ -17,14 +17,16 @@ var pool = mysql.createPool({
   user     : 'root',
   password : 'root',
   database : 'books'
-});
-/*var connection = mysql.createConnection({
+});*/
+var pool = mysql.createPool({
+multipleStatements: true,
+connectionLimit : 3,
 host     : process.env.MYSQL_SERVICE_HOST,
 user     : 'userJJI',
 password : 'root',
 port     : process.env.MYSQL_SERVICE_PORT,
 database : 'books'
-});*/
+});
 Object.assign=require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
@@ -80,9 +82,9 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
-//app.listen(port, ip);
-app.listen(8080, '127.0.0.1');
-console.log('server running');
-//console.log('Server running on http://%s:%s', ip, port);
+app.listen(port, ip);
+//app.listen(8080, '127.0.0.1');
+//console.log('server running');
+console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
